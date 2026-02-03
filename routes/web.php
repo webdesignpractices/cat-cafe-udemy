03355dae9284b068062503c208e174050145b0c7
ar <officebusiness800@gmail.com>
2026-02-03 22:23:37 +0900

    バリデーションtoログファイル

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 // Route::get('/', function () {
 //     return view('index');
 // });
 Route::view('/','index');
+Route::get('/contact',[ContactController::class,'index'])->name('contact');
+Route::post('/contact',[ContactController::class,'sendMail']);
+
+Route::get('/contact/complete',[ContactController::class,'complete'])->name('contact.complete');
+
