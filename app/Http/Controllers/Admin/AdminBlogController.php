7dd09acb339ad7b49b687300a9a33f9a3f9b2c11
ar <officebusiness800@gmail.com>
2026-02-11 18:55:06 +0900

    クエリビルダーを使い日付の新しい順で10件のブログ記事のみを表示した

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -17,7 +17,7 @@ class AdminBlogController extends Controller
      */
     public function index()
     {
-        $blogs = Blog::all();
+        $blogs = Blog::latest('updated_at')->limit(10)->get();
         return view('admin.blogs.index',['blogs' => $blogs]);
     }
 
