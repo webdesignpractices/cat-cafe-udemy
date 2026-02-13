831c91a6c7b5407d05ecf33227b9d51ef1f5c03f
ar <officebusiness800@gmail.com>
2026-02-13 09:40:38 +0900

    categoriesテーブルのIDを外部キーとしたblogsテーブルから->categoryとして値を取得できるようにするためにリレーション設定を行った

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
     protected $fillable = ['title','body','image'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Models;
+use App\Models\Blog;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function blogs(){
+        return $this->hasMany(Blog::class);
+    }
 }
