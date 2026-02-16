363c4bec260d92a68fad6148eb860ad6bb89079c
ar <officebusiness800@gmail.com>
2026-02-16 10:46:55 +0900

    intendedメソッドについて

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/contact/complete',[ContactController::class,'complete'])->name('con
 Route::get('/admin/blogs',[AdminBlogController::class,'index'])->name('admin.blogs.index')->middleware('auth');
 Route::get('/admin/blogs/create',[AdminBlogController::class,'create'])->name('admin.blogs.create');
 Route::post('/admin/blogs',[AdminBlogController::class,'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}',[AdminBlogController::class,'edit'])->name('admin.blogs.edit');
+Route::get('/admin/blogs/{blog}',[AdminBlogController::class,'edit'])->name('admin.blogs.edit')->middleware('auth');
 Route::put('/admin/blogs/{blog}',[AdminBlogController::class,'update'])->name('admin.blogs.update');
 Route::delete('/admin/blogs/{blog}',[AdminBlogController::class,'destroy'])->name('admin.blogs.destroy');
 //ユーザー登録
