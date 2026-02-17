e6926229ebef468c65df4bc015bc3270e0f04a8e
ar <officebusiness800@gmail.com>
2026-02-17 09:30:12 +0900

    ルートグループを活用し、パスが共通のルートで共通のミドルウェアをグループ化した

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,61 @@ Route::view('/','index');
 Route::get('/contact',[ContactController::class,'index'])->name('contact');
 Route::post('/contact',[ContactController::class,'sendMail']);
 Route::get('/contact/complete',[ContactController::class,'complete'])->name('contact.complete');
-//ブログ
-Route::get('/admin/blogs',[AdminBlogController::class,'index'])->name('admin.blogs.index')->middleware('auth');
-Route::get('/admin/blogs/create',[AdminBlogController::class,'create'])->name('admin.blogs.create');
-Route::post('/admin/blogs',[AdminBlogController::class,'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}',[AdminBlogController::class,'edit'])->name('admin.blogs.edit')->middleware('auth');
-Route::put('/admin/blogs/{blog}',[AdminBlogController::class,'update'])->name('admin.blogs.update');
-Route::delete('/admin/blogs/{blog}',[AdminBlogController::class,'destroy'])->name('admin.blogs.destroy');
-//ユーザー登録
-Route::get('/admin/users/create',[UserController::class,'create'])->name('admin.users.create');
-Route::post('/admin/users/store',[UserController::class,'store'])->name('admin.users.store');
 
-//認証
-Route::get('/admin/login',[AuthController::class,'showLoginForm'])->name('admin.login')->middleware('guest');
-Route::post('/admin/login',[AuthController::class,'login']);
-Route::post('/admin/logout',[AuthController::class,'logout'])->name('admin.logout');
+//管理画面のルートグループ
+// Route::prefix('/admin')//共通のパス
+//     ->name('admin.')//共通のルート名
+//     ->middleware('auth')//共通のミドルウェア
+//     ->group(function(){
+// //ブログ
+//         // Route::get('/blogs',[AdminBlogController::class,'index'])->name('blogs.index');
+//         // Route::get('/blogs/create',[AdminBlogController::class,'create'])->name('blogs.create');
+//         // Route::post('/blogs',[AdminBlogController::class,'store'])->name('blogs.store')->middleware('auth');
+//         // Route::get('/blogs/{blog}',[AdminBlogController::class,'edit'])->name('blogs.edit')->middleware('auth');
+//         // Route::put('/blogs/{blog}',[AdminBlogController::class,'update'])->name('blogs.update')->middleware('auth');
+//         // Route::delete('/blogs/{blog}',[AdminBlogController::class,'destroy'])->name('blogs.destroy');
+//         //resource一括登録
+//         Route::resource('/blogs',AdminBlogController::class)->except('show');
+        
+        
+//         //ユーザー登録
+//         Route::get('/users/create',[UserController::class,'create'])->name('users.create');
+//         Route::post('/users/store',[UserController::class,'store'])->name('users.store');
+
+//         //ログアウト       
+//         Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
 
+//     });
+//         Route::post('/admin/login',[AuthController::class,'login'])->middleware('guest');
+//         Route::get('/admin/login',[AuthController::class,'showLoginForm'])->name('admin.login')->middleware('guest');
+
+//記法2
+
+//管理画面のルートグループ
+Route::prefix('/admin')//共通のパス
+    ->name('admin.')//共通のルート名
+    
+    ->group(function(){
+        //ログイン時のみアクセス可能なルート
+        Route::middleware('auth')
+            ->group(function(){
+
+        //ブログ
+        Route::resource('/blogs',AdminBlogController::class)->except('show');
+                
+        //ユーザー登録
+        Route::get('/users/create',[UserController::class,'create'])->name('users.create');
+        Route::post('/users/store',[UserController::class,'store'])->name('users.store');
+
+        //ログアウト       
+        Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+
+        });
+        //未ログイン時のみアクセス可能なルート
+        Route::middleware('guest')
+            ->group(function(){
+        Route::post('/login',[AuthController::class,'login']);
+        Route::get('/login',[AuthController::class,'showLoginForm'])->name('login');
+        });
+    });
